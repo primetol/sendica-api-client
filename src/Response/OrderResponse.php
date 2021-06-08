438b04f52f14e3c5985c3ceb79a8269c6f54c091
AnatoliyBeketov <a.beketov@primecore.io>
2021-06-08 15:29:23 +0300

    orders response

--- src/Response/OrderResponse.php
+++ src/Response/OrderResponse.php
@@ -11,5 +11,9 @@ class OrderResponse extends AbstractResponse
         if ($this->type === 'order') {
             $this->data = new Order($this->response['data']);
         }
+
+        if ($this->type === 'order_pagination') {
+            $this->data = array_map(function($o) { return new Order($o); }, $this->response['data']['list']);
+        }
     }
 }
